4256590e144390e3871de8b6f00de0892a7a4866
Iftekhar A Chowdhury <007ifte@gmail.com>
2019-01-28 17:16:37 -0500

    Components Part 1

--- App.js
+++ App.js
@@ -1,21 +1,11 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Main from './components/MainComponent';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Hello World, this is Iftekhar Chowdhury. Welcome to my first react native app!</Text>
-      </View>
+      <Main />
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#2ff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
