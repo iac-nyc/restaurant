69c52ce44d4314109c9fa76dd3fe6bde6bc4fad4
Iftekhar A Chowdhury <007ifte@gmail.com>
2019-02-02 18:42:43 -0500

    Secure Store

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -6,6 +6,7 @@ import Contact from './ContactComponent';
 import Dishdetail from './DishdetailComponent';
 import Reservation from './ReservationComponent';
 import Favorites from './FavoriteComponent';
+import Login from './LoginComponent';
 import { View, Platform, Text, ScrollView, Image, StyleSheet } from 'react-native';
 import { createStackNavigator, createDrawerNavigator, DrawerItems, SafeAreaView } from 'react-navigation';
 import { Icon } from 'react-native-elements';
@@ -133,6 +134,7 @@ const AboutNavigator = createStackNavigator(
         onPress={ () => navigation.navigate('DrawerToggle') } />    
     })
   });
+
 const FavoritesNavigator = createStackNavigator({
     Favorites: { screen: Favorites }
   }, {
@@ -147,6 +149,23 @@ const FavoritesNavigator = createStackNavigator({
       headerLeft: <Icon name="menu" size={24}
         iconStyle={{ color: 'white' }} 
         onPress={ () => navigation.navigate('DrawerToggle') } />    
+    })
+  });
+
+const LoginNavigator = createStackNavigator({
+      Login: { screen: Login }
+    }, {
+    navigationOptions: ({ navigation }) => ({
+      headerStyle: {
+          backgroundColor: "#512DA8"
+      },
+      headerTitleStyle: {
+          color: "#fff"            
+      },
+      headerTintColor: "#fff",
+      headerLeft: <Icon name="menu" size={24}
+        iconStyle={{ color: 'white' }} 
+        onPress={ () => navigation.toggleDrawer() } />    
     })
   });
 
@@ -168,6 +187,21 @@ const FavoritesNavigator = createStackNavigator({
   );
 
 const MainNavigator = createDrawerNavigator({
+     Login: 
+  { screen: LoginNavigator,
+    navigationOptions: {
+      title: 'Login',
+      drawerLabel: 'Login',
+      drawerIcon: ({ tintColor, focused }) => (
+        <Icon
+          name='sign-in'
+          type='font-awesome'            
+          size={24}
+          iconStyle={{ color: tintColor }}
+        />
+      ),
+    }
+  },
     Home: 
       { screen: HomeNavigator,
         navigationOptions: {
@@ -262,8 +296,9 @@ const MainNavigator = createDrawerNavigator({
       },
   
     
-}, {
- drawerBackgroundColor: '#D1C4E9',
+},{
+  initialRouteName: 'Home',
+  drawerBackgroundColor: '#D1C4E9',
   contentComponent: CustomDrawerContentComponent
 });
 
