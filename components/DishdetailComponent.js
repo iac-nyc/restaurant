105dd4531195a45dd389e9df21be6134753303a8
Iftekhar A Chowdhury <007ifte@gmail.com>
2019-02-02 10:38:58 -0500

    left to right gesture added

--- components/DishdetailComponent.js
+++ components/DishdetailComponent.js
@@ -32,6 +32,12 @@ function RenderDish(props) {
         else
             return false;
     }
+  const recognizeComment = ({ moveX, moveY, dx, dy }) => {
+        if (dx > 200)
+            return true;
+        else
+            return false;
+    }
    
  const panResponder = PanResponder.create({
         onStartShouldSetPanResponder: (e, gestureState) => {
@@ -44,7 +50,7 @@ function RenderDish(props) {
         },
         onPanResponderEnd: (e, gestureState) => {
             console.log("pan responder end", gestureState);
-            if (recognizeDrag(gestureState))
+            if (recognizeDrag(gestureState)){
                 Alert.alert(
                     'Add Favorite',
                     'Are you sure you wish to add ' + dish.name + ' to favorite?',
@@ -54,6 +60,9 @@ function RenderDish(props) {
                     ],
                     { cancelable: false }
                 );
+          } else if (recognizeComment(gestureState)) {
+                props.toggleModal();
+            };
 
             return true;
         }
